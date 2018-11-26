6407045336f69d979aa425b788627c970ba38fb7
Patricio <pvalenzuela@nectia.com>
2018-11-26 11:40:08 -0300

    [Graphql] query data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 import ListRecetas from '../components/ListRecetas'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <ListRecetas/>
+    <ListRecetas data={data}/>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query IndexRecetas { 
+    allNodeRecipe {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+    }
+  }`   
 
 export default IndexPage
